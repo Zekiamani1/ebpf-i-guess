05c6ad0cfb49a7cfab9d9a3fa3943e6e81eaf793
zekiamani1 <zekiamanioke@gmail.com>
2026-08-12 20:30:01 +0700

    feat: event data using ring buffer i guess

--- main.c
+++ main.c
@@ -3,9 +3,21 @@
 #include <stdlib.h>
 #include <bpf/libbpf.h>
 #include <bpf/bpf.h>
-struct Data{
-    __u64  count;
-};
+typedef struct {
+    __u32 pid;
+    char name[16];
+    __u32 cpu_id;
+    __u64 timestamp;
+    long syscall_id;
+    __u64 args[6];
+} Data;
+static int f(void *ctx, void *data, size_t data_sz) {
+    const struct Data *ini = data;
+    printf("=====================================\n");
+    printf("syscall pid: %u \ncpu core: %u \nsyscall id: %ld \ntime: %llu ns\n", ini->pid, ini->cpu_id, ini->syscall_id, ini->timestamp);
+    printf("=====================================\n");
+    return 0;
+}
 int main() {
     struct bpf_object *obj;
     int err;
@@ -23,21 +35,26 @@ int main() {
     bpf_object__for_each_program(prog, obj) {
         bpf_program__attach(prog);
     }
-    int map_fd = bpf_object__find_map_fd_by_name(obj, "stats_map");
+    int count_map_fd = bpf_object__find_map_fd_by_name(obj, "count_map");
+    int data_map_fd = bpf_object__find_map_fd_by_name(obj, "data_map");
     int num_cpus = libbpf_num_possible_cpus();
     Data syscall_vals[num_cpus];
     Data sched_vals[num_cpus];
     __u32 key_sys = 0;
     __u32 key_sched = 1;
+    struct ring_buffer  rb = ring_buffer__new(data_map_fd, f, NULL, NULL);
     while (1) {
+        err = ring_buffer__poll(rb, 100);
         sleep(1);
-        bpf_map_lookup_elem(map_fd, &key_sys, syscall_vals);
-        bpf_map_lookup_elem(map_fd, &key_sched, sched_vals);
+        bpf_map_lookup_elem(count_map_fd, &key_sys, syscall_vals);
+        bpf_map_lookup_elem(count_map_fd, &key_sched, sched_vals);
         printf("=====================================\n");
         for (int i = 0; i < num_cpus; i++) {
-            printf("CPU Core %d \nSyscalls: %llu \nContext Switches: %llu\n", i, syscall_vals[i], sched_vals[i]);
+            printf("CPU Core %d \nSyscalls: %llu \nContext Switches: %llu\n", i, syscall_vals[i].count, sched_vals[i].count);
         }
         printf("=====================================\n");
     }
+    ring_buffer__free(rb);
+    bpf_object__close(obj);
     return 0;
 }
--- monitor.bpf.c
+++ monitor.bpf.c
@@ -1,22 +1,37 @@
 #include "vmlinux.h"
-#include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
-struct Data{
-    __u64  count;
+struct Data {
+    __u32 pid;
+    char name[16];
+    __u32 cpu_id;
+    __u64 timestamp;
+    long syscall_id;
+    __u64 args[6];
 };
 struct {
     __uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
     __uint(max_entries, 2);
     __type(key, __u32);
-    __type(value, struct Data);
-} stats_map SEC(".maps");
+    __type(value, __u64);
+} count_map SEC(".maps");
+struct {
+    __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+    __uint(max_entries, 256 * 1024);
+} data_map SEC(".maps");
 
 SEC("tp/raw_syscalls/sys_enter")
 int handle_sys_enter(struct trace_event_raw_sys_enter *ctx) {
     __u32 key = 0;
-    Data *data = bpf_map_lookup_elem(&stats_map, &key);
+    struct Data *data;
+    data = bpf_ringbuf_reserve(&data_map, sizeof(*data), 0);
+    __u64 *count = bpf_map_lookup_elem(&count_map, &key);
     if (data) {
-        data->count += 1;
+        *count += 1;
+        data->pid = bpf_get_current_pid_tgid() >> 32;
+        data->cpu_id = bpf_get_smp_processor_id();
+        data->timestamp = bpf_ktime_get_ns();
+        data->syscall_id = ctx->id;
+        bpf_ringbuf_submit(data, 0);
     }
     return 0;
 }
@@ -24,9 +39,9 @@ int handle_sys_enter(struct trace_event_raw_sys_enter *ctx) {
 SEC("tp/sched/sched_switch")
 int handle_sched_switch(void *ctx) {
     __u32 key = 1; 
-    Data *data = bpf_map_lookup_elem(&stats_map, &key);
+    __u64 *count = bpf_map_lookup_elem(&count_map, &key);
     if (data) {
-        data->count += 1; 
+        *count += 1; 
     }
     return 0;
 }
